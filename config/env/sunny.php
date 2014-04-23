d0b1418affe20851c2c4c798847c8d70c961c508
Sunny Luthra <sunny@mrova.com>
2014-04-23 08:58:54 +0530

    wp starter kit

---
+++ config/env/sunny.php
@@ -0,0 +1,53 @@
+<?php
+define('WP_HOME', 'http://chulbul.dev');
+define('WP_SITEURL', WP_HOME . '/wordpress/');
+
+define('WP_CONTENT_DIR', APP_ROOT . '/htdocs/content');
+define('WP_CONTENT_URL', WP_HOME . '/content');
+
+define('WP_DEBUG', false);
+define('FS_METHOD', 'direct');
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'test');
+
+/** MySQL database username */
+define('DB_USER', 'sunny');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost:3037');
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wpkit_';
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '');
+define('SECURE_AUTH_KEY',  '');
+define('LOGGED_IN_KEY',    '');
+define('NONCE_KEY',        '');
+define('AUTH_SALT',        '');
+define('SECURE_AUTH_SALT', '');
+define('LOGGED_IN_SALT',   '');
+define('NONCE_SALT',       '');
+
+/**#@-*/
